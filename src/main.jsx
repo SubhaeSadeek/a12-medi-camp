41cc08e00f0e740be593f43faec55656e76ff157
SubhaeSadeek <nurun.nabi@outlook.com>
2025-05-16 14:14:19 +0600

    router setup and Layout with outlet done

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
+import { RouterProvider } from "react-router-dom";
+import "./index.css";
+import AuthProvider from "./providers/AuthProvider";
+import router from "./routers/Routers";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<AuthProvider>
+			<QueryClientProvider client={queryClient}>
+				<HelmetProvider>
+					<div className="max-w-screen-xl mx-auto">
+						<RouterProvider router={router} />
+					</div>
+				</HelmetProvider>
+			</QueryClientProvider>
+		</AuthProvider>
+	</React.StrictMode>
+);
---
+++ src/pages/Home.jsx
@@ -0,0 +1,5 @@
+const Home = () => {
+	return <div>this is HOMEEEEEEEEEEEEEEEEEEEEE</div>;
+};
+
+export default Home;
