aa792ff2a5cfcd91653a2c19b5565b278ddeb460
SubhaeSadeek <nurun.nabi@outlook.com>
2025-05-18 23:53:51 +0600

    firbase hosting done

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-	baseURL: "http://localhost:5001",
+	baseURL: "https://a12-server-lyart.vercel.app",
 });
 
 const useAxiosPublic = () => {
